67b01a0985e1cde885db64bf7fd6790e0c2a0aed
aditya9u <aditya9ujalambkar@gmail.com>
2026-06-22 10:16:43 +0530

    initial Project setup

---
+++ expense-tracker/src/main/java/com/example/expense_tracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.example.expense_tracker.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
